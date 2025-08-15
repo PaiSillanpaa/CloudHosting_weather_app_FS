c21cdce288686e57836f846b95a0d35566d18310
PaiSillanpaa <sillanpaapai@gmail.com>
2025-08-15 14:25:33 +0300

    az yhteys luotu

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,14 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!"); // using anonymous function
-app.MapGet("/hellous", HelloMethod);
+app.MapGet("/hellous", GetHello);
 
 app.Run();
 
-string HelloMethod()
+string GetHello()
 {
-    var message = File.ReadAllText("hello.txt");
+    var helloFolder = new DirectoryInfo(Directory.GetCurrentDirectory());
+    var helloPath = Path.Combine(helloFolder.FullName, "hello.txt");
 
-    // soon we add code, which reads the message from code
+    // alternative shorter syntax ( same result )
+
+    var helloPath2 = Path.Combine(Directory.GetCurrentDirectory(), "hello.txt");
+
+    // print to console for debugging purposes
+    Console.WriteLine($"Reading hello from {helloPath}");
+
+    var message = File.ReadAllText("hello.txt");
     return "Read from FILE:\n\n" + message;
 }
