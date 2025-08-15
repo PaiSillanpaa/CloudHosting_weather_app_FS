afc06d7b29cdcb57701f0f963d5dac298238ee34
PaiSillanpaa <sillanpaapai@gmail.com>
2025-08-15 12:43:11 +0300

    testi file toimii

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!"); // using anonymous function
+app.MapGet("/hellous", HelloMethod);
 
 app.Run();
+
+string HelloMethod()
+{
+    var message = File.ReadAllText("hello.txt");
+
+    // soon we add code, which reads the message from code
+    return "Read from FILE:\n\n" + message;
+}
