cd713d973c478a824f415fe01589d0331e90654b
PaiSillanpaa <sillanpaapai@gmail.com>
2025-08-22 12:10:52 +0300

    add comments to code etc

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,10 +1,23 @@
+// 1
+
 var builder = WebApplication.CreateBuilder(args);
+
+// 2
+//we may need to configure builder before building ( as example, add controllers etc. )
+
+// 3
 var app = builder.Build();
 
+//4
+// MAP. ENDPOINT <-> metodi()
 app.MapGet("/", () => "Hello World!"); // using anonymous function
 app.MapGet("/hellous", GetHello);
 
+// 5 after run... program will stop here to wait for GET/POST/UPDATE calls...
 app.Run();
+
+Console.WriteLine("this should never happen...");
+// we will never get here...
 
 string GetHello()
 {
@@ -21,3 +34,7 @@ string GetHello()
     var message = File.ReadAllText("hello.txt");
     return "Read from FILE:\n\n" + message;
 }
+
+
+// Deploy with:
+// az webapp up --name my-very-best-net24s-chapp -g test1 --location westeurope --sku B1 --os-type linux
