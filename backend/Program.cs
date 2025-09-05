fddb11d887cfe22055c21057d73ba07d11ac6078
PaiSillanpaa <sillanpaapai@gmail.com>
2025-09-05 13:54:52 +0300

    improved error handling and something else

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,9 @@
-string path = Directory.GetCurrentDirectory();
-Console.WriteLine("The current directory is {0}", path);
+
+Console.WriteLine(@$"Current Directory: 
+{Directory.GetCurrentDirectory()}");
+
+Console.WriteLine(@$"Executing Assembly: 
+{Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)}");
 
 // 1
 
@@ -24,15 +28,23 @@ Console.WriteLine("this should never happen...");
 
 string GetHello()
 {
-    var helloFolder = new DirectoryInfo(Directory.GetCurrentDirectory());
+    // look for all assets in the same folder, where the program (dll's etc. ) are
+    // usually bin/Debug/net9.0) 
+    var helloFolder = new DirectoryInfo(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location));
+
     var helloPath = Path.Combine(helloFolder.FullName, "hello.txt");
 
     // alternative shorter syntax ( same result )
 
-    var helloPath2 = Path.Combine(Directory.GetCurrentDirectory(), "hello.txt");
+    // var helloPath2 = Path.Combine(Directory.GetCurrentDirectory(), "hello.txt");
+
+    if (!File.Exists(helloPath))
+    {
+        return @$"Sorry, Can't find anything. Couldn't find {helloPath} . Please copy hello.txt to the folder with the executable.";
+    }
 
     // print to console for debugging purposes
-    Console.WriteLine($"Reading hello from {helloPath}");
+    Console.WriteLine($"FILE FOUND! Yippee!Reading hello from {helloPath}");
 
     var message = File.ReadAllText("hello.txt");
     return "Read from FILE:\n\n" + message;
